bd4ff1c4f785c63f537a4500a23bee1323053398
naeunb <better903@naver.com>
2021-02-03 16:38:42 +0900

    변수사용법

---
+++ Java_005/src/com/callor/var/Var_03.java
@@ -0,0 +1,35 @@
+package com.callor.var;
+
+public class Var_03 {
+	public static void main(String[] args) {
+		int intNum1 = 0;
+		
+		intNum1 = 30;
+		System.out.println(intNum1);
+		System.out.println(intNum1 + 40);
+		
+		int intNum2 = 0;
+		intNum2 = 40;
+		
+		// intNum1 변수 값을 읽어오고
+		// intNum2 변수 값을 읽어와서
+		// 두 값을 덧셈한 후
+		// 어딘가에 저장
+		// 그리고, 어딘가에 저장된 값을
+		// console에 출력하라
+		System.out.println(intNum1 + intNum2);
+
+		int intSum = 0;
+
+		// intNum1 변수값을 읽어오고
+		// intNum2 변수값을 읽어와서
+		// 두 값을 덧셈한 후
+		// 위에서 선언하고 초기화 해 둔
+		// intSum 변수에 저장
+		intSum = intNum1 + intNum2;
+		// 그리고 intSum 변수에 저장된 값을
+		// 읽어서 console에 출력
+		System.out.println(intSum);
+		
+	}
+}
