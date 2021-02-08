57261fea7077f5ce1b7db51faa1c2e45a64fb2cc
naeunb <better903@naver.com>
2021-02-08 14:53:11 +0900

    multi

--- Java_008/src/com/callor/start/Multiple_01.java
+++ Java_008/src/com/callor/start/Multiple_01.java
@@ -4,14 +4,32 @@ public class Multiple_01 {
 	public static void main(String[] args) {
 		// 7~106까지 3의 배수들의합
 		
-		int num=0;
+		int sum=0;
 		for(int i=0; i<100; i++) {
 	
-			if((i+7)%3==0) {
-				num += (i+7);
-				System.out.println(i+7);
+			int num = i+7;
+			
+			if(num%3==0) {
+				sum += num;
+				System.out.println(num);
 			}
 		}
-		System.out.println(num);
+		System.out.println(sum);
+		
+		
+		
+		int int3M = 0;
+		for(int i=0; i<100; i++) {
+			int num = 1+7;
+			boolean bYes = num%3 == 0;
+
+			// if(num%3==0) {
+			// if(bYes == true) {
+			if(bYes) {
+				int3M += num;
+				System.out.println(num + " : 3의 배수");
+			}
+		}
+		System.out.println("3의 배수합 : " + int3M);
 	}
 }
---
+++ Java_008/src/com/callor/start/Multiple_02.java
@@ -0,0 +1,61 @@
+package com.callor.start;
+
+public class Multiple_02 {
+	public static void main(String[] args) {
+		
+		//7~106 까지 3의 배수가 아닌수들의 합
+		int sum = 0;
+		for(int i=0; i<100; i++) {
+
+			int num = i+7;
+			boolean bYes = num%3 == 0;
+			
+			if(!bYes) {
+				sum += num;
+			}
+			
+		}
+		System.out.println(sum);
+		
+		
+		int intSum = 0;
+		for(int i=0; i<100; i++) {
+			int num = i + 7;
+			boolean bYes  = num%3>0;
+			if(bYes) {
+				intSum += num;
+			}
+		}
+		System.out.println(intSum);
+
+		
+		int intSum1 = 0;
+		for(int i=0; i<100; i++) {
+			int num = i + 7;
+			boolean bYes  = num%3 != 0; // => num%3 == 1  &&  num%3 == 2
+			if(bYes) {
+				intSum1 += num;
+			}
+		}
+		System.out.println(intSum1);
+		
+		
+		
+		int intSum2 = 0;
+
+		for(int i = 0 ; i < 100; i++) {
+			int num = i + 7;
+			boolean bYes1 = num % 3 == 1;
+			if(bYes1) {
+				intSum2 += num;
+			}
+			boolean bYes2 = num % 3 == 2;
+			if(bYes2) {
+				intSum2 += num;
+			}
+		}
+		System.out.println(intSum2);
+		
+		
+	}
+}
