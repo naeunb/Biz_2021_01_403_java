ad7058e83b4d7799095d3eee3a5760bf1d5b84af
naeunb <better903@naver.com>
2021-02-24 09:03:04 +0900

    homework

--- Java_015/src/com/callor/applications/HomeWork_01.java
+++ Java_015/src/com/callor/applications/HomeWork_01.java
@@ -8,22 +8,22 @@ public class HomeWork_01 {
 	public static void main(String[] args) {
 		
 		Random rnd = new Random();
-		int[] num = new int[10];
+		int[] num = new int[100];
 		int i;
 		for(i=0; i<num.length; i++) {
 			num[i] = rnd.nextInt(10)+2;
-			System.out.println("랜덤으로 10개의 수 생성 : "+num[i]);
+			//System.out.println("랜덤으로 "+num.length+"개의 수 생성 : "+num[i]);
 		}
 		
 		PrimeServiceV2 psV2 = new PrimeServiceV2();
 
 		int sum = 0;
 		System.out.println("================");
-		for(i=2; i<num.length; i++) {
+		System.out.println("===소수리스트===");
+		System.out.println("================");
+		for(i=0; i<num.length; i++) {
 			int result = psV2.prime(num[i]);
-			if(result <0) {
-				
-			} else {
+			if(result >0) {
 				System.out.println(result+" : 소수");
 				sum += result;
 			}
